0cb6c90461805d895b7ba4ce5df0a24b94d9c7ee
Tomáš Kukrál <tomkukral@users.noreply.github.com>
2017-09-26 21:42:38 +0200

    add basic test for __main__

--- tests.py
+++ tests.py
@@ -1,3 +1,18 @@
-
 def test_dummy():
     assert True
+
+
+def test_main(monkeypatch):
+    global started
+    started = False
+
+    def fake_run():
+        global started
+        started = True
+
+    monkeypatch.setattr('kqueen.server.run', fake_run)
+
+    from kqueen import __main__
+    print(dir(__main__))
+
+    assert started, 'run() not executed'
