80655c3ae5987c67fd1afec31acdd1648d359a57
Tomáš Kukrál <tomkukral@users.noreply.github.com>
2017-10-01 21:20:04 +0200

    add local cluster to devenv

--- devenv.py
+++ devenv.py
@@ -5,14 +5,28 @@ from kqueen.models import Cluster
 import requests
 import yaml
 
-uuid = 'd3d0404c-202e-42b3-88d6-099abdc3d9e9'
+uuid_jenkins = 'd3d0404c-202e-42b3-88d6-099abdc3d9e9'
+uuid_local = 'd3d0404c-202e-42b3-88d6-099abdc3d9e0'
 kubeconfig_url = 'https://ci.mcp.mirantis.net/job/deploy-aws-k8s_ha_calico/199/artifact/kubeconfig'
 
 
+try:
+    cluster = Cluster(
+        id=uuid_jenkins,
+        name='testing',
+        provisioner='Manual',
+        kubeconfig=yaml.load(requests.get(kubeconfig_url).text),
+    )
+    cluster.save()
+except:
+    print('Adding aws cluster failed')
+
+
 cluster = Cluster(
-    id=uuid,
-    name='testing',
+    id=uuid_local,
+    name='local_cluster',
     provisioner='Manual',
-    kubeconfig=yaml.load(requests.get(kubeconfig_url).text),
+    kubeconfig=yaml.load(open('kubeconfig_localhost', 'r').read()),
 )
+print(cluster.kubeconfig.value)
 cluster.save()
