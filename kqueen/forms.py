8c9550c3c5d50f4906d442653eab2aaa1c7338ed
Ales Komarek <ales.komarek@newt.cz>
2017-10-02 11:54:06 +0200

    Got rid of DeprecationWarning

--- kqueen/forms.py
+++ kqueen/forms.py
@@ -1,19 +1,19 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired
 
 
-class LoginForm(Form):
+class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
 
 
-class ProvisionerCreateForm(Form):
+class ProvisionerCreateForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
     access_id = StringField('Access ID', validators=[DataRequired()])
     access_key = StringField('Access key', validators=[DataRequired()])
 
 
-class ClusterCreateForm(Form):
+class ClusterCreateForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
     provisioner = StringField('Provisioner', validators=[DataRequired()])
