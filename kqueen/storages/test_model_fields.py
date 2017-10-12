397afa20841dd7e3e22f1b42385dda6998df183d
Tomáš Kukrál <tomkukral@users.noreply.github.com>
2017-10-12 21:47:26 +0200

    fix for entangling
    There was a problem with class heredity and attributes was shared in
    Class instead of instances. I'm changing intialization sequence to
    mitigate this.
    
    see https://en.wikipedia.org/wiki/Entangled_(Red_Dwarf)

--- kqueen/storages/test_model_fields.py
+++ kqueen/storages/test_model_fields.py
@@ -57,6 +57,7 @@ class TestRequiredFields:
 class TestModelInit:
     def setup(self):
         self.model = create_model()
+        self.obj = self.model(**model_kwargs)
 
     @pytest.mark.parametrize('field_name,field_value', model_kwargs.items())
     def test_init_string(self, field_name, field_value):
@@ -72,7 +73,7 @@ class TestModelInit:
     def test_field_property_getters(self, attr, group):
         attr_name = '{}{}'.format(group, attr)
 
-        assert hasattr(self.model, attr_name)
+        assert hasattr(self.obj, attr_name)
 
 
 class TestGetFieldNames:
@@ -137,19 +138,23 @@ class TestSerialization:
 
 
 class TestDuplicateId:
-    def test_write(self):
-        model = create_model()
+    def setup(self):
+        self.model = create_model()
 
-        obj1_kwargs = {'string': 'object 1', 'json': {'a': 1, 'b': 2, 'c': 'tri'}, 'secret': 'pass'}
-        obj2_kwargs = {'string': 'object2', 'json': {'a': 1, 'b': 2, 'c': 'tri'}, 'secret': 'pass'}
+        self.obj1_kwargs = {'string': 'object 1', 'json': {'a': 1, 'b': 2, 'c': 'tri'}, 'secret': 'pass'}
+        self.obj2_kwargs = {'string': 'object 2', 'json': {'a': 1, 'b': 2, 'c': 'tri'}, 'secret': 'pass'}
 
-        obj1 = model(**obj1_kwargs)
+    def test_with_save(self):
+        """"Save object are not same"""
+        obj1 = self.model(**self.obj1_kwargs)
+        obj2 = self.model(**self.obj2_kwargs)
+
+        assert obj1 != obj2
+
         obj1.save()
-        obj2 = model(**obj2_kwargs)
         obj2.save()
 
         print(obj1.get_dict())
         print(obj2.get_dict())
 
-        assert obj1 != obj2
         assert obj1.id != obj2.id
