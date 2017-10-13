4898d003f3f661c3ab4e0d97d293b752a6cf6ad4
Tomáš Kukrál <tomkukral@users.noreply.github.com>
2017-10-13 10:31:43 +0200

    use cluster kqueen-testing for in devenv script

--- devenv.py
+++ devenv.py
@@ -10,7 +10,7 @@ uuid_jenkins = 'c88b05d6-a107-4636-a3cc-eb5c90562f8f'
 uuid_local = '2d51891a-adac-4bbc-a725-eed20cc67849'
 uuid_provisioner_jenkins = 'e8de24b0-43d1-4a3c-af55-7b1d3f700554'
 uuid_provisioner_local = '203c50d6-3d09-4789-8b8b-1ecb00814436'
-kubeconfig_url = 'https://ci.mcp.mirantis.net/job/deploy-aws-k8s_ha_calico/199/artifact/kubeconfig'
+kubeconfig_url = 'https://ci.mcp.mirantis.net/job/deploy-aws-k8s_ha_calico_sm/33/artifact/kubeconfig'
 
 
 try:
