2982f88dcebda88585a308cc001654a9aab23e8f
Adam Tengler <atengler@mirantis.com>
2017-10-13 14:02:47 +0200

    Jenkins Engine init test

---
+++ kqueen/engines/test_jenkins.py
@@ -0,0 +1,17 @@
+import pytest
+
+from .jenkins import JenkinsEngine
+
+
+class TestJenkinsInit:
+    def setup(self):
+        pass
+
+    def test_init_jenkins(self, cluster):
+        engine = JenkinsEngine(cluster)
+
+    def test_init_jenkins_with_kwargs(self, cluster):
+        engine = JenkinsEngine(cluster, username='foo', password='bar')
+        assert engine.username == 'foo'
+        assert engine.password == 'bar'
+
