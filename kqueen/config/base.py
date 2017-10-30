c09341c5d43247e8d28956c333f116b3ea40e6a7
Tomáš Kukrál <tomkukral@users.noreply.github.com>
2017-10-30 10:03:34 +0100

    implement get and to_dict methods

--- kqueen/config/base.py
+++ kqueen/config/base.py
@@ -24,3 +24,25 @@ class BaseConfig:
     PROVISIONER_ERROR_STATE = 'Error'
     PROVISIONER_OK_STATE = 'OK'
     PROVISIONER_UNKNOWN_STATE = 'Not Reachable'
+
+    @classmethod
+    def get(cls, name, default=None):
+        """Emulate get method from dict"""
+
+        if hasattr(cls, name):
+            return getattr(cls, name)
+        else:
+            return default
+
+    @classmethod
+    def to_dict(cls):
+        """Return dict of all uppercase attributes"""
+
+        out = {}
+
+        for att_name in dir(cls):
+            if att_name.isupper():
+                out[att_name] = getattr(cls, att_name)
+
+        return out
+
