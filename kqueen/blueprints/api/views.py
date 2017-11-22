99e6b5549e4b4719f1d3d8e93d3622c68b45798f
Adam Tengler <atengler@mirantis.com>
2017-11-22 20:28:07 +0100

    Remove unnecessary split

--- kqueen/blueprints/api/views.py
+++ kqueen/blueprints/api/views.py
@@ -162,9 +162,8 @@ def engine_list():
     module_path = 'kqueen.engines'
     for engine in ENGINES:
         try:
-            class_name = engine.split('.')[-1]
             module = import_module(module_path)
-            _class = getattr(module, class_name)
+            _class = getattr(module, engine)
             engine_cls.append({
                 'name': '.'.join([module_path, engine]),
                 'parameters': _class.get_parameter_schema()
