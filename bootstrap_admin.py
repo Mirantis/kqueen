6ea2d6c7d6ebb5c885020174a32d4bc588568a83
Adam Tengler <atengler@mirantis.com>
2017-12-01 14:51:54 +0100

    Add bootstrap admin script

---
+++ bootstrap_admin.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+from datetime import datetime
+from kqueen.models import Organization, User
+from kqueen.server import create_app
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("organization", help="Organization name")
+parser.add_argument("namespace", help="Organization namespace")
+parser.add_argument("username", help="Admin username")
+parser.add_argument("password", help="Admin password")
+
+organization_id = '22d8df64-4ac9-4be0-89a7-c45ea0fc85da'
+user_id = '09587e34-812d-4efc-af17-fbfd7315674c'
+
+
+def main():
+    args = parser.parse_args()
+    app = create_app()
+    with app.app_context():
+        # Organization and user
+        try:
+            organization = Organization(
+                id=organization_id,
+                name=args.organization,
+                namespace=args.namespace,
+                created_at=datetime.utcnow()
+            )
+            organization.save()
+        except:
+            raise Exception('Adding {} organization failed'.format(args.organization))
+        try:
+            user = User(
+                None,
+                id=user_id,
+                username=args.username,
+                password=args.password,
+                organization=organization,
+                created_at=datetime.utcnow(),
+                active=True
+            )
+            user.save()
+        except:
+            raise Exception('Adding {} user failed'.format(args.username))
+
+
+if __name__ == "__main__":
+    main()
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
 export prometheus_multiproc_dir="$(mktemp -d)"
+BOOTSTRAP_ADMIN_USERNAME="${BOOTSTRAP_ADMIN_USERNAME:-admin}"
+BOOTSTRAP_ADMIN_PASSWORD="${BOOTSTRAP_ADMIN_PASSWORD:-default}"
+BOOTSTRAP_ADMIN_ORGANIZATION="${BOOTSTRAP_ADMIN_ORGANIZATION:-DemoOrg}"
+BOOTSTRAP_ADMIN_NAMESPACE="${BOOTSTRAP_ADMIN_NAMESPACE:-demoorg}"
+
+if [ "$BOOTSTRAP_ADMIN" = true ] ; then
+    python bootstrap_admin.py ${BOOTSTRAP_ADMIN_ORGANIZATION} ${BOOTSTRAP_ADMIN_NAMESPACE} ${BOOTSTRAP_ADMIN_USERNAME} ${BOOTSTRAP_ADMIN_PASSWORD}
+fi
 
 exec gunicorn --config kqueen/gunicorn.py kqueen.wsgi
