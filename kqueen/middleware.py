c8ae585f9a0a17d63d4835f4b045dc64bc5bc869
Tomáš Kukrál <tkukral@mirantis.com>
2017-12-27 12:53:55 +0100

    don't prometheus_multiproc_dir when gunicorn isn't used

--- kqueen/middleware.py
+++ kqueen/middleware.py
@@ -30,13 +30,21 @@ def record_request_data(response):
 
     return response
 
+def check_prometheus():
+    is_gunicorn = "gunicorn" in os.environ.get("SERVER_SOFTWARE", "")
 
-def setup_metrics(app):
-    # TODO: detect multiprocess mode and raise only when needed
+    if not is_gunicorn:
+        return True
+
     if 'prometheus_multiproc_dir' in os.environ:
         os.makedirs(os.environ['prometheus_multiproc_dir'], exist_ok=True)
     else:
         raise Exception('Please set prometheus_multiproc_dir variable using `export prometheus_multiproc_dir=$(mktemp -d)`')
+
+
+def setup_metrics(app):
+
+    check_prometheus()
 
     app.before_request(start_timer)
     app.after_request(record_request_data)
