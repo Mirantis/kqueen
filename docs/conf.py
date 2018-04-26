0e22eaef76fdd3ec79995b15070d62c5fe312a23
Ekaterina Chernova <efedorova@mirantis.com>
2018-04-26 16:07:27 +0300

    Handle RTD build with C-dependent libriaries

--- docs/conf.py
+++ docs/conf.py
@@ -18,9 +18,20 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 # import os
-# import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import sys
+from unittest.mock import MagicMock
+
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+
+MOCK_MODULES = ['ldap']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration ------------------------------------------------
 
