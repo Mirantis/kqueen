47b499718f312f970300f612ae3a0eb0825dfe4a
vnaumov <vnaumov@mirantis.com>
2018-06-07 21:09:52 +0400

    Refactor EKS
    - Template structure defined
    - Kubeconfig download fixed
    - Cluster states fixed
    - Engine status fixed
    - TODO added
    
    current issue related with kqueen-k8s-api methods:
    ```
    api_1           | Traceback (most recent call last):
    api_1           |   File "/code/kqueen/models.py", line 124, in status
    api_1           |     'addons': kubernetes.list_services(filter_addons=True),
    api_1           |   File "/code/kqueen/kubeapi.py", line 238, in list_services
    api_1           |     include_uninitialized=include_uninitialized
    api_1           |   File "/usr/local/lib/python3.6/site-packages/kubernetes/client/apis/core_v1_api.py", line 14358, in list_service_for_all_namespaces
    api_1           |     (data) = self.list_service_for_all_namespaces_with_http_info(**kwargs)
    api_1           |   File "/usr/local/lib/python3.6/site-packages/kubernetes/client/apis/core_v1_api.py", line 14455, in list_service_for_all_namespaces_with_http_info
    api_1           |     collection_formats=collection_formats)
    api_1           |   File "/usr/local/lib/python3.6/site-packages/kubernetes/client/api_client.py", line 321, in call_api
    api_1           |     _return_http_data_only, collection_formats, _preload_content, _request_timeout)
    api_1           |   File "/usr/local/lib/python3.6/site-packages/kubernetes/client/api_client.py", line 155, in __call_api
    api_1           |     _request_timeout=_request_timeout)
    api_1           |   File "/usr/local/lib/python3.6/site-packages/kubernetes/client/api_client.py", line 342, in request
    api_1           |     headers=headers)
    api_1           |   File "/usr/local/lib/python3.6/site-packages/kubernetes/client/rest.py", line 231, in GET
    api_1           |     query_params=query_params)
    api_1           |   File "/usr/local/lib/python3.6/site-packages/kubernetes/client/rest.py", line 222, in request
    api_1           |     raise ApiException(http_resp=r)
    api_1           | kubernetes.client.rest.ApiException: (403)
    ```
    faced when try to parse k8s config body

--- kqueen/engines/__init__.py
+++ kqueen/engines/__init__.py
@@ -2,5 +2,6 @@ from .jenkins import JenkinsEngine
 from .manual import ManualEngine
 from .gce import GceEngine
 from .aks import AksEngine
+from .eks import EksEngine
 
 __all__ = ['JenkinsEngine', 'ManualEngine', 'GceEngine', 'AksEngine', 'EksEngine']
--- kqueen/engines/eks.py
+++ kqueen/engines/eks.py
@@ -3,6 +3,7 @@ from kqueen.engines.base import BaseEngine
 import boto3
 
 import logging
+import pkgutil
 import yaml
 
 logger = logging.getLogger('kqueen_api')
@@ -13,7 +14,7 @@ STATE_MAP = {
     'ACTIVE': config.get('CLUSTER_OK_STATE'),
     'DELETING': config.get('CLUSTER_DEPROVISIONING_STATE'),
     'FAILED': config.get('CLUSTER_ERROR_STATE'),
-    'UPDATED': config.get('CLUSTER_UPDATING_STATE')
+    'UPDATING': config.get('CLUSTER_UPDATING_STATE')
 }
 
 
@@ -40,38 +41,72 @@ class EksEngine(BaseEngine):
                 'validators': {
                     'required': True
                 }
+            },
+            'aws_region': {
+                'type': 'select',
+                'label': 'AWS Region',
+                'order': 2,
+                'choices': [
+                    ('us-east-1', 'US East (N. Virginia)'),
+                    ('us-east-2', 'US East (Ohio)'),
+                    ('us-west-1', 'US West (N. California)'),
+                    ('us-west-2', 'US West (Oregon)'),
+                    ('ca-central-1', 'Canada (Central)'),
+                    ('eu-central-1', 'EU (Frankfurt)'),
+                    ('eu-west-1', 'EU (Ireland)'),
+                    ('eu-west-2', 'EU (London)'),
+                    ('eu-west-3', 'EU (Paris)'),
+                    ('ap-northeast-1', 'Asia Pacific (Tokyo)'),
+                    ('ap-northeast-2', 'Asia Pacific (Seoul)'),
+                    ('ap-northeast-3', 'Asia Pacific (Osaka-Local)'),
+                    ('ap-southeast-1', 'Asia Pacific (Singapore)'),
+                    ('ap-southeast-2', 'Asia Pacific (Sydney)'),
+                    ('ap-south-1', 'Asia Pacific (Mumbai)'),
+                    ('sa-east-1', 'South America (São Paulo)')
+                ],
+                'validators': {
+                    'required': True
+                }
             }
         },
         'cluster': {
             'node_count': {
+            # TODO unsupported, need to define template k8s input
                 'type': 'integer',
                 'label': 'Node Count',
                 'default': 3,
+                'order': 3,
                 'validators': {
                     'required': True,
                     'min': 1,
                     'number': True
                 }
             },
-            'roleArn': {
+            'role_arn': {
                 'type': 'text',
                 'label': 'IAM Role ARN',
+                'order': 4,
                 'validators': {
                     'required': True
                 }
             },
-            'subnetid': {
+            'subnet_id': {
+            # TODO subnetIds list - Amazon EKS requires subnets in at least two Availability Zones.
+            # TODO subnetIds must be attached to common security-group.
                 'type': 'text',
                 'label': 'Subnet Id',
+                'order': 5,
                 'validators': {
                     'required': True
                 }
             },
-            'securitygroupid': {
+            'security_group_id': {
+            # TODO securityGroupIds (list) -- Specify one or more security groups
                 'type': 'text',
                 'label': 'Security Group Id',
+                'order': 6,
                 'validators': {
-                    'required': True
+                    'required': False
                 }
             }
         }
@@ -84,11 +119,29 @@ class EksEngine(BaseEngine):
         # Call parent init to save cluster on self
         super(EksEngine, self).__init__(cluster, **kwargs)
         # Client initialization
-        self.aws_access_key = kwargs.get('aws_access_key', '')
-        self.aws_secret_key = kwargs.get('aws_secret_key', '')
+        self.aws_access_key = kwargs['aws_access_key']
+        self.aws_secret_key = kwargs['aws_secret_key']
+        self.aws_region = kwargs['aws_region']
         self.client = self._get_client()
+        # Cluster settings
+        self.role_arn = kwargs['role_arn']
+        subnets = kwargs['subnet_id']
+        self.subnet_id = subnets.split(',').strip()
+        security_groups = kwargs.get('security_group_id', '')
+        self.security_group_id = security_groups.split(',').strip()
+        # Get templates
+        files = self._get_template_files()
+        self.eks_kubeconfig = files['template.yaml']
         # Cache settings
         self.cache_timeout = 5 * 60
+
+    def _get_template_files(self):
+        package_name = "kqueen.engines.resources.aws"
+        files = {}
+        entities = ['template']
+        for f in entities:
+            files[f + ".yaml"] = pkgutil.get_data(package_name, f + ".yaml")
+        return files
 
     def _get_client(self):
         """
@@ -96,6 +149,7 @@ class EksEngine(BaseEngine):
         """
         client = boto3.client(
             'eks',
+            region_name=self.aws_region,
             aws_access_key_id=self.aws_access_key,
             aws_secret_access_key=self.aws_secret_key
         )
@@ -105,31 +159,21 @@ class EksEngine(BaseEngine):
         """
         Implementation of :func:`~kqueen.engines.base.BaseEngine.provision`
         """
-        # self.name = kwargs.get('name', 'noname')
         try:
             response = self.client.create_cluster(
-                name=self.cluster.name,
-                roleArn=self.cluster.roleArn,
+                name=self.cluster.id,
+                roleArn=self.role_arn,
                 resourcesVpcConfig={
-                    'subnetIds': [
-                        self.cluster.subnetid
-                    ],
-                    'securityGroupIds': [
-                        self.cluster.securitygroupid
-                    ]
+                    'subnetIds': self.subnet_id,
+                    'securityGroupIds': self.security_group_id
                 }
             )
-             
-            self.cluster.metadata['endpoint'] = response['cluster']['endpoint']
-            self.cluster.metadata['roleArn'] = response['cluster']['roleArn']
-            self.cluster.metadata['status'] = response['cluster']['status'] 
-            self.cluster.metadata['id'] = response['cluster']['name']
-            self.cluster.save()
+
             # TODO: check if provisioning response is healthy
         except Exception as e:
             msg = 'Creating cluster {} failed with the following reason:'.format(self.cluster.id)
             logger.exception(msg)
-            return False, msg
+            return False, e
         return True, None
 
     def deprovision(self, **kwargs):
@@ -141,12 +185,12 @@ class EksEngine(BaseEngine):
         if result:
             return result, error
         try:
-            self.client.delete_cluster(name=self.cluster.metadata['id'])
+            self.client.delete_cluster(name=self.cluster.id)
             # TODO: check if deprovisioning response is healthy
         except Exception as e:
             msg = 'Deleting cluster {} failed with the following reason:'.format(self.cluster.id)
             logger.exception(msg)
-            return False, msg
+            return False, e
         return True, None
 
     def resize(self, node_count, **kwargs):
@@ -159,16 +203,19 @@ class EksEngine(BaseEngine):
         Implementation of :func:`~kqueen.engines.base.BaseEngine.get_kubeconfig`
         """
         if not self.cluster.kubeconfig:
-            cluster = self.client.describe_cluster(name=self.cluster.metadata['id'])
+            cluster = self.client.describe_cluster(name=self.cluster.id)
             kubeconfig = {}
             if cluster['cluster']['status'] != "ACTIVE":
                 return self.cluster.kubeconfig
-            self.cluster.kubeconfig = yaml.load(open("eks-kubeconfig").read()) 
+            self.cluster.kubeconfig = yaml.load(self.eks_kubeconfig)
             self.cluster.kubeconfig["clusters"][0]["cluster"] = {
-                "server": cluster['endpoint'], 
-                "certificate-authority-data": cluster['certificateAuthority']['data']
+                "server": cluster['cluster']['endpoint'],
+                "certificate-authority-data": cluster['cluster']['certificateAuthority']['data']
             }
-            self.cluster.kubeconfig["users"][0]["user"]["exec"]["args"][2] = cluster['name']
+            self.cluster.kubeconfig["users"][0]["user"]["exec"]["args"][2] = cluster['cluster']['name']
+            # TODO define , do we need to use user Arn or Cluster Arn
+#            self.cluster.kubeconfig["users"][0]["user"]["exec"]["args"][4] = cluster['cluster']['roleArn']
+#            self.cluster.kubeconfig["users"][0]["user"]["exec"]["args"][4] = cluster['cluster']['arn']
             self.cluster.save()
         return self.cluster.kubeconfig
 
@@ -178,9 +225,9 @@ class EksEngine(BaseEngine):
         """
         response = {}
         try:
-            response = self.client.describe_cluster(self.cluster.metadata['id'])
+            response = self.client.describe_cluster(name=self.cluster.id)
         except Exception as e:
-            msg = 'Fetching data from backend for cluster {} failed with the following reason:'.format(self.cluster.metadata['heat_cluster_id'])
+            msg = 'Fetching data from backend for cluster {} failed with the following reason:'.format(self.cluster.id)
             logger.exception(msg)
             return {}
         state = STATE_MAP.get(response['cluster']['status'], config.get('CLUSTER_UNKNOWN_STATE'))
@@ -202,13 +249,15 @@ class EksEngine(BaseEngine):
     @classmethod
     def engine_status(cls, **kwargs):
         try:
-            aws_access_key = kwargs.get('aws_access_key', '')
-            aws_secret_key = kwargs.get('aws_secret_key', '')
-        except Exception:
-            logger.exception('{} Eks Provisioner validation failed.'.format(cls.name))
+            aws_access_key = kwargs['aws_access_key']
+            aws_secret_key = kwargs['aws_secret_key']
+            aws_region = kwargs['aws_region']
+        except KeyError:
             return config.get('PROVISIONER_ERROR_STATE')
+
         client = boto3.client(
             'eks',
+            region_name=aws_region,
             aws_access_key_id=aws_access_key,
             aws_secret_access_key=aws_secret_key
         )
@@ -217,4 +266,5 @@ class EksEngine(BaseEngine):
         except Exception:
             logger.exception('{} Eks Provisioner validation failed.'.format(cls.name))
             return config.get('PROVISIONER_UNKNOWN_STATE')
+
         return config.get('PROVISIONER_OK_STATE')
