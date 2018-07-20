c8d3dcd6a45ee4daa577956eb71a23ce423e81fd
Ekaterina Chernova <efedorova@mirantis.com>
2018-07-20 17:30:37 +0300

    Remove pycrypto usage
    * Use cryptography package instaed
    * Update piplock.file to be sync with new dependencies

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
         'oauth2client==3.0.0',
         'openstacksdk==0.16.0',
         'python-neutronclient==6.9.0',
-        'pycrypto',
+        'cryptography==2.3',
         'prometheus_client',
         'python-etcd',
         'python-jenkins==0.4.16',
